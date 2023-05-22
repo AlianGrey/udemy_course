aec9f6bea6742d58ca061584e8b15f254c8ddddb
Elena <ek371116@gmail.com>
2023-05-22 22:19:31 +0400

    functions

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+// Место для первой задачи
+function sayHello(name) {
+    let str = (`Привет, ${name}!`)
+    return str;
+}
+
+console.log(sayHello('Антон'));
+
+
+// Место для второй задачи
+function returnNeighboringNumbers(num) {
+    let arr = [num - 1, num, num + 1];
+    return arr;
+}
+
+console.log(returnNeighboringNumbers(5));
+
+// Место для третьей задачи
+function getMathResult(num, count) {
+    if (typeof count != 'number' || count <= 0) {
+        return num;
+    }
+    let str = '';
+    for (let i = 1; i <= count; i++) {
+        if (i === count) {
+            return str += num * i;
+        }
+        str += num * i + '--';
+    }
+}
+console.log(getMathResult(3, 10));
